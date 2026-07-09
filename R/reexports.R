a822e1cf3c6e79d3322a7786a3a17a59b2c5423d
Aleš Vomáčka <61730945+alesvomacka@users.noreply.github.com>
2026-07-09 20:37:16 +0200

    exported spreadview functions

---
+++ R/reexports.R
@@ -0,0 +1,7 @@
+#' @importFrom spreadview get_categorical_vars
+#' @export
+spreadview::get_categorical_vars
+
+#' @importFrom spreadview compose_spreadsheet
+#' @export
+spreadview::compose_spreadsheet
